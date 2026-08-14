135184653660394fa9899f4e6746281fe05467e4
huytr06205 <huyt07464@gmail.com>
2026-08-14 18:54:45 +0700

    Delete bài 7.8.9 directory

--- bài 7.8.9/bai9.py
+++
@@ -1,5 +0,0 @@
-nums = [int(input(f"Nhập số thứ {i+1}: ")) for i in range(3)]
-tong = sum(nums)
-print("Tổng là:", tong)
-so_chan = sum(1 for x in str(tong) if int(x) % 2 == 0)
-print("Số chữ số chẵn trong tổng là:", so_chan)
